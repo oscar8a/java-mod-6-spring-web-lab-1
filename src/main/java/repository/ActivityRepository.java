4a19600dcd89b25375ce099e03a079bec21b1985
Oscar 8A <oscarochoacerrato@gmail.com>
2022-08-05 15:36:39 -0400

    commit each repository file

--- src/main/java/repository/ActivityRepository.java
+++ src/main/java/repository/ActivityRepository.java
@@ -1,2 +1,10 @@
-package repository;public interface ActivityRepository {
+package repository;
+
+import model.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    Activity findByName(String name);
+
+    void deleteByName(String name);
 }
--- src/main/java/repository/SignUpRepository.java
+++ src/main/java/repository/SignUpRepository.java
@@ -1,2 +1,10 @@
-package repository;public interface SignUpRepository {
+package repository;
+
+import model.SignUp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SignUpRepository extends JpaRepository<SignUp, Long> {
+    SignUp findByName(String name);
+
+    void deleteByName(String name);
 }
